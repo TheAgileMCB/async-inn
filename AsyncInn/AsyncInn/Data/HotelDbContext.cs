f14be4fc604ff728e3292c06360d23fdbea2d47d
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-20 20:49:46 -0500

    successful migration!

--- AsyncInn/AsyncInn/Data/HotelDbContext.cs
+++ AsyncInn/AsyncInn/Data/HotelDbContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AsyncInn.Data
 {
-    public class HotelDbContext
+    public class HotelDbContext : DbContext
     {
+        public HotelDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //protected override void OnModelCreation
+
+            public DbSet<Hotel> Hotel { get; set; }
     }
 }
