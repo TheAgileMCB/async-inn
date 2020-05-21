10aa4f50cba8d1719ee1193fc83d9979945b4e21
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-21 10:20:20 -0500

    migration of refactored tables. (money type to HotelRoom / enum to Room)

--- AsyncInn/AsyncInn/Migrations/20200521151929_AddRoomAmenityTableAndOtherTablesRefactored.Designer.cs
+++ AsyncInn/AsyncInn/Migrations/20200521151929_AddRoomAmenityTableAndOtherTablesRefactored.Designer.cs
@@ -4,14 +4,16 @@ using AsyncInn.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AsyncInn.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200521151929_AddRoomAmenityTableAndOtherTablesRefactored")]
+    partial class AddRoomAmenityTableAndOtherTablesRefactored
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AsyncInn/AsyncInn/Migrations/20200521151929_AddRoomAmenityTableAndOtherTablesRefactored.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class AddRoomAmenityTableAndOtherTablesRefactored : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HotelRoom",
+                table: "HotelRoom");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "HotelRoom");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomID",
+                table: "HotelRoom",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "HotelRoom",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "HotelRoom",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HotelRoom",
+                table: "HotelRoom",
+                columns: new[] { "HotelID", "Number" });
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenity",
+                columns: table => new
+                {
+                    RoomID = table.Column<int>(nullable: false),
+                    AmenityID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenity", x => new { x.RoomID, x.AmenityID });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenity_Amenity_AmenityID",
+                        column: x => x.AmenityID,
+                        principalTable: "Amenity",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenity_Room_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Room",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRoom_RoomID",
+                table: "HotelRoom",
+                column: "RoomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenity_AmenityID",
+                table: "RoomAmenity",
+                column: "AmenityID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HotelRoom_Hotel_HotelID",
+                table: "HotelRoom",
+                column: "HotelID",
+                principalTable: "Hotel",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HotelRoom_Room_RoomID",
+                table: "HotelRoom",
+                column: "RoomID",
+                principalTable: "Room",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HotelRoom_Hotel_HotelID",
+                table: "HotelRoom");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HotelRoom_Room_RoomID",
+                table: "HotelRoom");
+
+            migrationBuilder.DropTable(
+                name: "RoomAmenity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HotelRoom",
+                table: "HotelRoom");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HotelRoom_RoomID",
+                table: "HotelRoom");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "HotelRoom");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomID",
+                table: "HotelRoom",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "HotelRoom",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomNumber",
+                table: "HotelRoom",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HotelRoom",
+                table: "HotelRoom",
+                columns: new[] { "HotelID", "RoomNumber" });
+        }
+    }
+}
