e35b32f4d3899b1ce0b32e80564f55ab0af09031
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-21 18:18:53 -0500

    migrated because I thought something had changed. It had not

---
+++ AsyncInn/AsyncInn/Migrations/20200521231803_BuiltApiControllers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class BuiltApiControllers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
