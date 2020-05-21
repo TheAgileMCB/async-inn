f24215b64ba998110694dc9ffd62091858f56a55
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-21 08:33:19 -0500

    built room table

---
+++ AsyncInn/AsyncInn/Models/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public int Layout { get; set; }
+    }
+}
