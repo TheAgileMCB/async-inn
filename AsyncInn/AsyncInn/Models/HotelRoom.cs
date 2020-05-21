b1b34b67033f4ce937583fe5e913b99af7c28ec8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-21 08:33:44 -0500

    built hotelroom table

--- AsyncInn/AsyncInn/Models/HotelRoom.cs
+++ AsyncInn/AsyncInn/Models/HotelRoom.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,6 +9,9 @@ namespace AsyncInn.Models
 {
     public class HotelRoom
     {
+        public int HotelID { get; set; }
+        public int RoomID { get; set; }
+
         [Required]
         public int RoomNumber { get; set; }
         [Required]
