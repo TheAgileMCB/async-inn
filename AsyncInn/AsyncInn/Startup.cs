e5fdfee77ee215c8cfc40eb0cdebe30c85e719f8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-26 16:35:11 -0500

    added Transient call to Startup

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AsyncInn.Data;
+using AsyncInn.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +32,8 @@ namespace AsyncInn
                 string connectionString = Configuration.GetConnectionString("DefaultConnection");
                 options.UseSqlServer(connectionString);
             });
+
+            services.AddTransient<IHotelService, HotelService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
