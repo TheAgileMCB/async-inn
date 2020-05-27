a7d152268c7777ef4cd7922046b4813276e74719
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-26 19:03:08 -0500

    created room interface

---
+++ AsyncInn/AsyncInn/Data/Services/IRoomService.cs
@@ -0,0 +1,21 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data.Services
+{
+    interface IRoomService
+    {
+        Task<IEnumerable<Room>> GetAllHotels();
+
+        Task<Room> GetOneHotel(int ID);
+
+        Task<bool> UpdateHotel(int ID, Room room);
+
+        Task<Room> AddHotel(Room room);
+
+        Task<Room> DeleteHotel(int ID);
+    }
+}
