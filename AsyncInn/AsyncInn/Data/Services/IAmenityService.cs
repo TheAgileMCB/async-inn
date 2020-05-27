da0b3dcafa2a364cc0636cf90a39c4d278df3ca6
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-26 19:52:59 -0500

    built Amenity interface file

---
+++ AsyncInn/AsyncInn/Data/Services/IAmenityService.cs
@@ -0,0 +1,21 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data.Services
+{
+    public interface IAmenityService
+    {
+        Task<IEnumerable<Amenity>> GetAllAmenitys();
+
+        Task<Amenity> GetOneAmenity(int ID);
+
+        Task<bool> UpdateAmenity(int ID, Amenity Amenity);
+
+        Task<Amenity> AddAmenity(Amenity Amenity);
+
+        Task<Amenity> DeleteAmenity(int ID);
+    }
+}
