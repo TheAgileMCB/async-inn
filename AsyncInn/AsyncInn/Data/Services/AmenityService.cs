6d1b103a2a4c48b0e2ee5349215e60a5e9f4f80a
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-26 19:53:50 -0500

    scaffolded AmenityService file

---
+++ AsyncInn/AsyncInn/Data/Services/AmenityService.cs
@@ -0,0 +1,36 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data.Services
+{
+    public class AmenityService : IAmenityService
+    {
+        public Task<Amenity> AddAmenity(Amenity Amenity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Amenity> DeleteAmenity(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Amenity>> GetAllAmenitys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Amenity> GetOneAmenity(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAmenity(int ID, Amenity Amenity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
