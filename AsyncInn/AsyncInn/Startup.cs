ce7e991e3924bfd20b51d5dab9c4f1f54d2c10da
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-26 19:48:18 -0500

    added amenity transient service to startup

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -35,6 +35,7 @@ namespace AsyncInn
 
             services.AddTransient<IHotelService, HotelService>();
             services.AddTransient<IRoomService, RoomService>();
+            services.AddTransient<IAmenityService, AmenityService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
