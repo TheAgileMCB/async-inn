3767b4687e0b801b12f75b6a3139b3c9aa7535a2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-26 19:45:42 -0500

    added transient call for rooms to startup

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -34,6 +34,7 @@ namespace AsyncInn
             });
 
             services.AddTransient<IHotelService, HotelService>();
+            services.AddTransient<IRoomService, RoomService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
