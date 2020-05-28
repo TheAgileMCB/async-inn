2d8f606916158cbc3c55b4efb3b4dcf66dfa75e8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-27 19:43:15 -0500

    created HotelRoomDTO file

---
+++ AsyncInn/AsyncInn/Models/Api/HotelRoomDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Api
+{
+    public class HotelRoomDTO
+    {
+        public int HotelID { get; set; }
+        public int Number { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+        public Room Room { get; set; }
+    }
+}
