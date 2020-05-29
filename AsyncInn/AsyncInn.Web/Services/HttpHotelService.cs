51e2b6d4299d36311433170394b27eca04e2b2f9
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 17:08:30 -0500

    built and implemented interface for GetAll and GetOne Hotel

---
+++ AsyncInn/AsyncInn.Web/Services/HttpHotelService.cs
@@ -0,0 +1,30 @@
+﻿using AsyncInn.Web.Models;
+using AsyncInn.Web.Models.Services;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Web
+{
+    public class HttpHotelService : IHotelService
+    {
+        private static readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new System.Uri("https://localhost:5001/api/"),
+        };
+        public async Task<List<Hotel>> GetAll()
+        {
+            var responseStream = await client.GetStreamAsync("Hotels");
+            List<Hotel> result = await JsonSerializer.DeserializeAsync<List<Hotel>>(responseStream);
+            return result;
+        }
+
+        public async Task<Hotel> GetOne(int id)
+        {
+            var responseStream = await client.GetStreamAsync($"Hotels/{id}");
+            Hotel result = await JsonSerializer.DeserializeAsync<Hotel>(responseStream);
+            return result;
+        }
+    }
+}
---
+++ AsyncInn/AsyncInn.Web/Services/IHotelService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Web.Models.Services
+{
+    public interface IHotelService
+    {
+        Task<List<Hotel>> GetAll();
+        Task<Hotel> GetOne(int id);
+    }
+}
