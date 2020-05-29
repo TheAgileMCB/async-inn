fa0bce17e73266c32c99a8843489d699eab0bb13
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 17:06:24 -0500

    added properties to extract from Json

--- AsyncInn/AsyncInn.Web/Models/Hotel.cs
+++ AsyncInn/AsyncInn.Web/Models/Hotel.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AsyncInn.Web.Models
 {
     public class Hotel
     {
+        [JsonPropertyName("id")]
         public int ID { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; }
         [Display(Name = "Street Address")]
+        [JsonPropertyName("streetAddress")]
         public string StreetAddress { get; set; }
+        [JsonPropertyName("city")]
         public string City { get; set; }
+        [JsonPropertyName("state")]
         public string State { get; set; }
+        [JsonPropertyName("country")]
         public string Country { get; set; }
+        [JsonPropertyName("phone")]
         public string Phone { get; set; }
         public List<HotelRoomSummary> Rooms { get; set; }
     }
