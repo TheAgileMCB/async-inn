ece8ee6ff5bbd4c8bd476605da446823fd4c4a2b
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 14:42:42 -0500

    set some model display properties

--- AsyncInn/AsyncInn.Web/Models/HotelRoomSummary.cs
+++ AsyncInn/AsyncInn.Web/Models/HotelRoomSummary.cs
@@ -1,11 +1,16 @@
-﻿namespace AsyncInn.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsyncInn.Web.Models
 {
     public class HotelRoomSummary
     {
         public int HotelID { get; set; }
         public int Number { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Rate { get; set; }
+        [Display(Name = "Pet Friendly")]
         public bool PetFriendly { get; set; }
+        [Display(Name = "Room ID")]
         public int RoomID { get; set; }
         public RoomSummary Room { get; set; }
     }
