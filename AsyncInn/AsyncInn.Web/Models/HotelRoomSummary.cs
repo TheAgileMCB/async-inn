af96b36e2e826fb81f03e77d3159edc5276b4c35
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 08:41:47 -0500

    added hotelroom model

---
+++ AsyncInn/AsyncInn.Web/Models/HotelRoomSummary.cs
@@ -0,0 +1,12 @@
+﻿namespace AsyncInn.Web.Models
+{
+    public class HotelRoomSummary
+    {
+        public int HotelID { get; set; }
+        public int Number { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+        public int RoomID { get; set; }
+        public RoomSummary Room { get; set; }
+    }
+}
