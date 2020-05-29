b7ec7484f752f8f4b156535463ae06870d5727a3
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 08:42:12 -0500

    added room model

---
+++ AsyncInn/AsyncInn.Web/Models/RoomSummary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AsyncInn.Web.Models
+{
+    public class RoomSummary
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Layout { get; set; }
+
+        public List<AmenitySummary> Amenities { get; set; }
+    }
+}
