e9f72af78d8d5091646b49f389ecdd87b960ab2d
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 08:41:18 -0500

    added hotel model

---
+++ AsyncInn/AsyncInn.Web/Models/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Web.Models
+{
+    public class Hotel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public List<HotelRoomSummary> Rooms { get; set; }
+    }
+}
