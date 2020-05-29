ff873a8ccd4de6e15724d7cbdeaf69fb886d341b
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-29 11:18:35 -0500

    added CreateHotelRoom model to handle hotel room creation

---
+++ AsyncInn/AsyncInn/Models/CreateHotelRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.API.Models
+{
+    public class CreateHotelRoom
+    {
+        public int Number { get; set; }
+        public decimal Rate { get; set; }
+        public int RoomID {get; set;}
+    }
+}
