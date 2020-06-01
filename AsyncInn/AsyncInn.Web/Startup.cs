4142964f317f86a69cf420101da2288f94b448ef
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-31 19:02:19 -0500

    edited Startup + for eventual deployment

--- AsyncInn/AsyncInn.Web/Startup.cs
+++ AsyncInn/AsyncInn.Web/Startup.cs
@@ -32,7 +32,7 @@ namespace AsyncInn.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (true /*env.IsDevelopment()*/)
             {
                 app.UseDeveloperExceptionPage();
             }
